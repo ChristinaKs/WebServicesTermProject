17f84dd8b61d09013f94a3e1078607863ab57218
ChristinaKs <74264367+ChristinaKs@users.noreply.github.com>
2022-11-16 17:02:25 -0500

    Final pathing for studios
    get -- /studios/{studio}/games

--- game-api/includes/routes/studio_routes.php
+++ game-api/includes/routes/studio_routes.php
@@ -6,9 +6,10 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . './../models/BaseModel.php';
 require_once __DIR__ . './../models/StudioModel.php';
+require_once __DIR__ . './../models/GameModel.php';
 
 // Callback for HTTP GET /studios
-//-- Supported filtering operation: By developer
+//-- Supported filtering operation: By developer, by pubisher and by location
 function handleGetAllStudios(Request $request, Response $response, array $args){
     $studios = array();
     $response_data = array();
@@ -188,6 +189,42 @@ function handleDeleteStudio(Request $request, Response $response, array $args) {
         $response_data = makeCustomJSONError("badRequest", "No studio id was provided.");
         $response->getBody()->write($response_data);
         return $response->withStatus(HTTP_BAD_REQUEST);
+    }
+    $response->getBody()->write($response_data);
+    return $response->withStatus($response_code);
+}
+
+// Callback for HTTP GET /studios/{studio_id}/games
+//-- Supported filtering parameters: GameName and GameId
+function handleGetGamesByStudioId(Request $request, Response $response, array $args) {
+    $games = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $game_model = new GameModel();
+
+    // Retreive the studio if from the request's URI.
+    $studio_id = $args["studio_id"];
+    
+    // Fetch the info about the specified games.
+    $filter_params = $request->getQueryParams();
+    if (isset($studio_id)) {
+        
+        $games = $game_model->getGamesByStudioId($studio_id);
+        if(!$games){ // No matches found?
+            $response_data = makeCustomJSONError("resourceNotFound", "No matching record was found for the specified studio.");
+            $response->getBody()->write($response_data);
+            return $response->withStatus(HTTP_NOT_FOUND);
+        }
+    }
+    // Handle serve-side content negotiation and produce the requested representation.    
+    $requested_format = $request->getHeader('Accept');
+    
+    //-- We verify the requested resource representation.    
+    if ($requested_format[0] === APP_MEDIA_TYPE_JSON) {
+        $response_data = json_encode($games, JSON_INVALID_UTF8_SUBSTITUTE);
+    } else {
+        $response_data = json_encode(getErrorUnsupportedFormat());
+        $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
     }
     $response->getBody()->write($response_data);
     return $response->withStatus($response_code);
--- game-api/index.php
+++ game-api/index.php
@@ -26,8 +26,11 @@ require_once './includes/routes/studio_routes.php';
 $app->get("/studios", "handleGetAllStudios");
 $app->post("/studios", "handleCreateStudio");
 $app->put("/studios", "handleUpdateStudio");
+
 $app->get("/studios/{studio_id}", "handleGetStudioById");
 $app->delete("/studios/{studio_id}", "handleDeleteStudio");
+
+$app->get("/studios/{studio_id}/games", "handleGetGamesByStudioId");
 
 // Run the app.
 $app->run();
